ba4f93fa615956cd58ade2381627303f47782c77
Danielle Neves <dnneves@icloud.com>
2024-10-09 11:00:49 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -415,7 +415,7 @@ window.addEventListener("DOMContentLoaded", function(e) {
 
     $(function() {
       typed.typed({
-        strings: ["Danielle Neves", "Product Designer"],
+        strings: ["I'm Danielle Neves,", "an Engineer", "and Product Designer."],
         typeSpeed: 100,
       responsiveClass: true,
         loop: true,
